4bf8774453b9063f8065b6a777693a1b98800bae
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-22 14:25:30 +0100

    add mikroOrm config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { MikroORM } from '@mikro-orm/core';
+import { Migration } from '@mikro-orm/migrations';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
