e3c88441f4ad0981c867b009055282395225ccba
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-22 15:59:36 +0100

    config apolloserver

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM, ServerException } from '@mikro-orm/core';
 import { Migration } from '@mikro-orm/migrations';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
-
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send('Hello World');
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+    }),
   });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({
+    app,
+    cors: { credentials: true, origin: true },
+  });
+
   app.listen(4000, () => {
     console.log('listening on 4000');
   });
 };
-
 main().catch((err) => {
   console.error(err);
 });
