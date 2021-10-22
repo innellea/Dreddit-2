d2231f231d6e9821a9adfa51cdd20ddd853ebe89
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-22 14:51:33 +0100

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import { Migration } from '@mikro-orm/migrations';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-  orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
+
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send('Hello World');
+  });
+  app.listen(4000, () => {
+    console.log('listening on 4000');
+  });
 };
 
 main().catch((err) => {
