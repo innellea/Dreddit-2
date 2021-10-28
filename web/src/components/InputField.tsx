2a588b16a2c0ce3314a68d11996884d83377abe0
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-10-28 14:03:45 +0100

    add register page

---
+++ web/src/components/InputField.tsx
@@ -0,0 +1,27 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { useField } from "formik";
+import React, { ReactElement, InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  placeholder?: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = (props) => {
+  const [field, { error }] = useField(props);
+  return (
+    <div>
+      <FormControl isInvalid={!!error}>
+        <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+        <Input {...field} id={field.name} placeholder={props.placeholder} />
+        {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+      </FormControl>
+    </div>
+  );
+};
