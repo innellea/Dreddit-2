df0478e96bc56e0ad8be97687b3b18092e7c23f0
innellea <rolandkiraly94@gmail.com>
2021-12-26 00:53:43 +0000

    add password reset

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,18 +13,21 @@ import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import cors from 'cors';
-import redis from 'redis';
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { MyContext } from './types';
 import sendEmail from './utils/sendEmails';
+
 const main = async () => {
-  sendEmail('bob@bob.com', 'hello there');
+  // sendEmail('bob@bob.com', 'hello there');
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
+
   const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+
+  const redis = new Redis();
   //TODO: Change cors policy to production
 
   app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
@@ -32,7 +35,7 @@ const main = async () => {
     session({
       name: COOKIE_NAME,
       store: new RedisStore({
-        client: redisClient,
+        client: redis,
         disableTouch: true,
         disableTTL: true,
       }),
@@ -53,7 +56,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
     // optional plugin for graphiQl playground
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
   });
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,11 +11,11 @@ import { MyContext } from '../types';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
 import { EntityManager } from '@mikro-orm/postgresql';
-import { COOKIE_NAME } from '../constants';
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
 import { validateRegister } from '../utils/validateRegister';
 import sendEmail from '../utils/sendEmails';
-
+import { v4 } from 'uuid';
 @ObjectType()
 class FieldError {
   @Field()
@@ -36,14 +36,24 @@ class UserResponse {
 @Resolver()
 export class UserResolver {
   @Mutation(() => Boolean)
-  async forgotPassword(@Arg('email') email: string, @Ctx() { em }: MyContext) {
+  async forgotPassword(
+    @Arg('email') email: string,
+    @Ctx() { em, redis }: MyContext
+  ) {
     const user = await em.findOne(User, { email });
     if (!user) {
       // the email is not in the database
       return false;
     }
 
-    const token = 'asd21ed';
+    const token = v4();
+
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      'ex',
+      1000 * 60 * 60 * 24 * 3
+    );
     await sendEmail(
       email,
       `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
