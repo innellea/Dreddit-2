5520d929ae42581b13efb0a6bf5d943b90b6d0a2
innellea <rolandkiraly94@gmail.com>
2022-01-22 19:10:16 +0000

    adding change-vote functionality

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -16,6 +16,8 @@ import {
 
 import { getConnection } from 'typeorm';
 
+import { tmpdir } from 'os';
+
 import { MyContext } from '../types';
 import { Post } from '../entities/Post';
 import { Updoot } from '../entities/Updoot';
@@ -54,25 +56,53 @@ export class PostResolver {
         const isUpdoot = value !== -1;
         const realValue = isUpdoot ? 1 : -1;
         const { userId } = req.session;
-        // await Updoot.insert({
-        //   userId,
-        //   postId,
-        //   value: realValue,
-        // });
-        await getConnection().query(
-            `
-      START TRANSACTION;
-  
+
+        const updoot = await Updoot.findOne({ where: { postId, userId } });
+
+        // the user has voted on the post before
+        // and they are changing their vote
+        if (updoot && updoot.value !== realValue) {
+            await getConnection().transaction(async (tm) => {
+                await tm.query(
+                    `
+      update updoot
+      set value = $1
+      where "postId" = $2 and "userId" = $3
+          `,
+                    [realValue, postId, userId]
+                );
+
+                await tm.query(
+                    `
+            update post
+            set points = points + $1
+            where id = $2
+          `,
+                    // changing vote
+                    [2 * realValue, postId]
+                );
+            });
+        } else if (!updoot) {
+            // has never voted before
+            await getConnection().transaction(async (tm) => {
+                await tm.query(
+                    `
       insert into updoot ("userId", "postId", value)
-      values (${userId},${postId},${realValue});
-  
+      values ($1, $2, $3)
+          `,
+                    [userId, postId, realValue]
+                );
+
+                await tm.query(
+                    `
       update post
-      set points = points + ${realValue}
-      where id = ${postId};
-  
-      COMMIT;
-      `
-        );
+      set points = points + $1
+      where id = $2
+        `,
+                    [realValue, postId]
+                );
+            });
+        }
         return true;
     }
 
